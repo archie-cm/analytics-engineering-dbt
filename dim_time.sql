fdb234d169b68cf565eb5fb16b5c8c6ddcf9cf40
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2023-03-14 12:39:06 +0700

    Update dim_time.sql

--- dim_time.sql
+++ dim_time.sql
@@ -1,17 +1,30 @@
+-- time_dimension.sql
 {{
-  config(
+
+config(
     materialized='table',
     unique_key='time_id'
-  )
+)
 }}
 
-select
-  trip_start_timestamp,
-  extract(year from trip_start_timestamp) as year,
-  extract(quarter from trip_start_timestamp) as quarter,
-  extract(month from trip_start_timestamp) as month,
-  extract(day from trip_start_timestamp) as day_of_month,
-  extract(dayofweek from trip_start_timestamp) as day_of_week,
-  extract(hour from trip_start_timestamp) as hour
-from
-  `bigquery-public-data.chicago_taxi_trips.taxi_trips`
+WITH time_data AS (
+  SELECT
+    DISTINCT
+    EXTRACT(DATE FROM trip_start_timestamp) AS trip_date,
+    EXTRACT(HOUR FROM trip_start_timestamp) AS trip_hour,
+    EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS day_of_week,
+    EXTRACT(MONTH FROM trip_start_timestamp) AS month,
+    EXTRACT(YEAR FROM trip_start_timestamp) AS year
+  FROM
+    `{{ ref('taxi_trips_fact') }}`
+)
+
+SELECT
+  ROW_NUMBER() OVER() AS time_id,
+  trip_date,
+  trip_hour,
+  day_of_week,
+  month,
+  year
+FROM
+  time_data
